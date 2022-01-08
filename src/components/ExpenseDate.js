9ab82ea665618b4913933d5ea60ec06d4ff91f2a
RahulFernando <rahulrulz680@gmail.com>
2022-01-08 11:26:04 +0530

    component splitting

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function ExpenseDate(props) {
+  const month = props.expense_date.toLocaleString('en-US', { month: 'long' });
+  const day = props.expense_date.toLocaleString('en-US', { day: '2-digit' });
+  const year = props.expense_date.getFullYear();
+
+  return (
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day ">{day}</div>
+    </div>
+  );
+}
+
+export default ExpenseDate;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,11 @@
 import React from 'react';
+import ExpenseDate from './ExpenseDate';
 import './style.css';
 
 function ExpenseItem(props) {
-  const  month = props.expense_date.toLocaleString('en-US', { month: 'long' });
-  const day = props.expense_date.toLocaleString('en-US', { day: '2-digit' });
-  const year = props.expense_date.getFullYear();
-
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate expense_date={props.expense_date} />
       <div className="expense-item__description">
         <h2>{props.expense_title}</h2>
         <div className="expense-item__price">${props.expense_amount}</div>
