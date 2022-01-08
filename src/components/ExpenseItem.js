6d955199cc379000165db9ec83b4dd755d19fa94
RahulFernando <rahulrulz680@gmail.com>
2022-01-08 11:13:50 +0530

    normal logic

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,9 +2,17 @@ import React from 'react';
 import './style.css';
 
 function ExpenseItem(props) {
+  const  month = props.expense_date.toLocaleString('en-US', { month: 'long' });
+  const day = props.expense_date.toLocaleString('en-US', { day: '2-digit' });
+  const year = props.expense_date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>{props.expense_date.toString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.expense_title}</h2>
         <div className="expense-item__price">${props.expense_amount}</div>
