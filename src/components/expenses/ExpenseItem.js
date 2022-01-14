0c2aa1c63e62189f7f5aade46a8b88e724e68991
RahulFernando <rahulrulz680@gmail.com>
2022-01-14 13:16:59 +0530

    use of states

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.expense_title);
+
   return (
     <div className="expense-item">
       <ExpenseDate expense_date={props.expense_date} />
       <div className="expense-item__description">
-        <h2>{props.expense_title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.expense_amount}</div>
       </div>
+      <button onClick={() => setTitle('Updated!!!')}>Change Title</button>
     </div>
   );
 }
