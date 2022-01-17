ecede82045986bbd2354eb401db713a9aa42e585
RahulFernando <rahulrulz680@gmail.com>
2022-01-17 17:39:42 +0530

    form hide

--- src/components/newExpense/ExpenseForm.js
+++ src/components/newExpense/ExpenseForm.js
@@ -22,6 +22,7 @@ const ExpenseForm = (props) => {
     };
     props.onSaveData(expenseData);
     setValues(obj);
+    props.onCancel();
   };
 
   return (
@@ -60,6 +61,7 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="type" onClick={props.onCancel}>Cancel</button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
--- src/components/newExpense/NewExpense.js
+++ src/components/newExpense/NewExpense.js
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseForm from './ExpenseForm';
 
 // style
 import './style.css';
 
 const NewExpense = (props) => {
-    const saveExpenseHandler = (data) => {
-        const expenseData = {
-            ...data,
-            id: Math.random().toPrecision()
-        }
+  const [visible, setVisible] = useState(false);
 
-        props.onAddExpense(expenseData);
-    }
+  const saveExpenseHandler = (data) => {
+    const expenseData = {
+      ...data,
+      id: Math.random().toPrecision(),
+    };
 
-    return (
-        <div className="new-expense">
-            <ExpenseForm onSaveData={saveExpenseHandler} />
-        </div>
-    );
-}
+    props.onAddExpense(expenseData);
+  };
+
+  const visibilityHandler = () => {
+    setVisible(!visible);
+  };
+
+  return (
+    <div className="new-expense">
+      {!visible && (
+        <button type="button" onClick={visibilityHandler}>
+          Add New Expense
+        </button>
+      )}
+      {visible && (
+        <ExpenseForm
+          onSaveData={saveExpenseHandler}
+          onCancel={visibilityHandler}
+        />
+      )}
+    </div>
+  );
+};
 
 export default NewExpense;
